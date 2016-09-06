30bf33d5abd93f94eb3447af2161a0c136d8e2e7
Ania <Ania>
2016-09-06 11:08:31 +0200

    before adding gallery

--- components/change-language/language.js
+++ components/change-language/language.js
@@ -1,28 +1,28 @@
 var ngApp = angular.module ('testApp'); // odnosimy sie do naszego modulu z pliku glownego
 
-ngApp.directive('changeLanguage', function(){
-	return {
-		templateUrl: 'components/change-language/language.html',
-		restrict: 'E',
-		scope: {}, 
-		controller: function($scope, gettextCatalog){
-			$scope.changeLanguage = function(language){
-			gettextCatalog.setCurrentLanguage(language);
-			document.title = gettextCatalog.getString('My Page - Ania Kazmierczak')
-			}
-			$scope.languageList = [
-			{	
-					code:'pl',
-					label:'Polski'
-				},
-				{
-					code:'en',
-					label:'English'
-				}
-			];
-		}
-	}
-});
+// ngApp.directive('changeLanguage', function(){
+// 	return {
+// 		templateUrl: 'components/change-language/language.html',
+// 		restrict: 'E',
+// 		scope: {}, 
+// 		controller: function($scope, gettextCatalog){
+// 			$scope.changeLanguage = function(language){
+// 			gettextCatalog.setCurrentLanguage(language);
+// 			document.title = gettextCatalog.getString('My Page - Ania Kazmierczak')
+// 			}
+// 			$scope.languageList = [
+// 			{	
+// 					code:'pl',
+// 					label:'Polski'
+// 				},
+// 				{
+// 					code:'en',
+// 					label:'English'
+// 				}
+// 			];
+// 		}
+// 	}
+// });
 
 ngApp.directive('changeLanguageImg', function () {
     return {
